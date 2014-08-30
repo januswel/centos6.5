297649cf6a550376114a6dc6718f303be5d9b1e5
janus_wel <janus.wel.3@gmail.com>
2014-08-30 12:59:54 +0900

    Add specs for MongoDB

---
+++ serverspec/spec/log/mongodb_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+%w(
+  mongodb-org
+  mongodb-org-server
+  mongodb-org-mongos
+  mongodb-org-shell
+  mongodb-org-tools
+).each do |name|
+  describe package(name) do
+    it { should be_installed }
+  end
+end
+
+describe service('mongod') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+# MongoDB should have a user "root"
+describe command("mongo admin -u root -p root --eval 'quit();'") do
+  it { should return_exit_status 0 }
+end
