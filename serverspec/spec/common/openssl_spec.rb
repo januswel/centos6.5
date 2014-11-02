7d822396bed2ec7974e1855f6b5f113cf27acdc3
janus_wel <janus.wel.3@gmail.com>
2014-11-03 03:22:09 +0900

    Update OpenSSL

--- serverspec/spec/common/openssl_spec.rb
+++ serverspec/spec/common/openssl_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 VERSION = '1.0.1e'
-RELEASE = '30.el6_5.2'
+RELEASE = '30.el6_6.2'
 
 describe package('openssl') do
   it { should be_installed.with_version(VERSION + '-' + RELEASE) }
